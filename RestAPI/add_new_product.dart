8b14e52a82d3ae6ad5b6c5d371be5b51043e60d5
Butcher-png <abhijit0045@gmail.com>
2025-08-27 12:11:38 +0600

    project

--- RestAPI/add_new_product.dart
+++ RestAPI/add_new_product.dart
@@ -8,11 +8,56 @@ class AddNewProduct extends StatefulWidget {
 }
 
 class _AddNewProductState extends State<AddNewProduct> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Add Data"),
+      appBar: AppBar(title: Text("Add Data")),
+      body: Form(
+        key: _formkey,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  label: Text("Product Name"),
+                  hintText: "Product Name",
+                ),
+              ),
+
+              SizedBox(height: 10,),
+              TextFormField(
+                decoration: InputDecoration(
+                  label: Text("Product Code"),
+                  hintText: "Product Code",
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextFormField(
+                decoration: InputDecoration(
+                  label: Text("Quantity"),
+                  hintText: "Quantity",
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextFormField(
+                decoration: InputDecoration(
+                  label: Text("Unit Price"),
+                  hintText: "Unit Price",
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextFormField(
+                decoration: InputDecoration(
+                  label: Text("Img URL"),
+                  hintText: "Img URL",
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
