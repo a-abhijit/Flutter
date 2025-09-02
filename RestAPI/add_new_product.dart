dc43823a717280449c245d86c999ba370b24690b
Abhijit Saha <abhijit0045@gmail.com>
2025-09-02 14:34:20 +0600

    project

--- RestAPI/add_new_product.dart
+++ RestAPI/add_new_product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class AddNewProduct extends StatefulWidget {
   const AddNewProduct({super.key});
@@ -8,6 +11,7 @@ class AddNewProduct extends StatefulWidget {
 }
 
 class _AddNewProductState extends State<AddNewProduct> {
+  bool _addInProgress = false;
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   final TextEditingController nameController =TextEditingController();
   final TextEditingController codeController =TextEditingController();
@@ -27,14 +31,19 @@ class _AddNewProductState extends State<AddNewProduct> {
             child: Column(
               children: [
                 TextFormField(
-                  keyboardType: TextInputType.text,
                   controller: nameController,
                   decoration: InputDecoration(
                     label: Text("Product Name"),
                     hintText: "Product Name",
                   ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty){
+                      return "Please enter a product name";
+                    }return null;
+                  },
                 ),
-        
+
+
                 SizedBox(height: 10,),
                 TextFormField(
                   keyboardType: TextInputType.text,
@@ -43,6 +52,11 @@ class _AddNewProductState extends State<AddNewProduct> {
                     label: Text("Product Code"),
                     hintText: "Product Code",
                   ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty){
+                      return "Please enter a product name";
+                    }return null;
+                  },
                 ),
                 SizedBox(height: 10,),
                 TextFormField(
@@ -52,6 +66,11 @@ class _AddNewProductState extends State<AddNewProduct> {
                     label: Text("Quantity"),
                     hintText: "Quantity",
                   ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty){
+                      return "Please enter a quantity";
+                    }return null;
+                  },
                 ),
                 SizedBox(height: 10,),
                 TextFormField(
@@ -61,6 +80,11 @@ class _AddNewProductState extends State<AddNewProduct> {
                     label: Text("Unit Price"),
                     hintText: "Unit Price",
                   ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty){
+                      return "Please enter unit price";
+                    }return null;
+                  },
                 ),
                 SizedBox(height: 10,),
                 TextFormField(
@@ -70,9 +94,21 @@ class _AddNewProductState extends State<AddNewProduct> {
                     label: Text("Img URL"),
                     hintText: "Img URL",
                   ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty){
+                      return "Please enter a img name";
+                    }return null;
+                  },
                 ),
                 SizedBox(height: 30,),
-                FilledButton(onPressed: (){}, child: Text("Add product"))
+                Visibility(
+                  visible: _addInProgress ==false,
+                  replacement: Center(child: CircularProgressIndicator()),
+                  child: FilledButton(onPressed: (){
+                    _onTapAddProductButton();
+
+                    }, child: Text("Add product")),
+                )
               ],
             ),
           ),
@@ -80,6 +116,59 @@ class _AddNewProductState extends State<AddNewProduct> {
       ),
     );
   }
+  void _onTapAddProductButton()async{
+    if (_formkey.currentState?.validate() == false){
+      return;
+    }
+    _addInProgress = true;
+    setState(() {});
+  //   prepare uri
+    Uri uri = Uri.parse('http://35.73.30.144:2008/api/v1/CreateProduct');
+    //prepare data
+    Map<String,dynamic> requestBody= {
+      "ProductName": nameController.text,
+      "ProductCode": codeController.text,
+      "Img": imgController.text,
+      "Qty": quantityController.text,
+      "UnitPrice": unitController.text,
+      "TotalPrice": int.parse(quantityController.text)*int.parse(unitController.text)
+    };
+    //request with data
+
+    Response response = await post(uri,
+        headers: {
+      'Content-Type':'application/json'
+        },
+        body: jsonEncode(requestBody));
+    print(response.statusCode);
+    print(response.body);
+
+    if(response.statusCode == 200){
+      final decodeJson= jsonDecode(response.body);
+      if (decodeJson["status"]=="success"){
+        clearField();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Data added successfully")));
+
+      }else{
+        clearField();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(decodeJson["data"])));
+
+      }
+    }
+    _addInProgress = false;
+    setState(() {});
+    
+  }
+
+  void clearField(){
+    nameController.clear();
+    codeController.clear();
+    quantityController.clear();
+    unitController.clear();
+    imgController.clear();
+  }
+
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -90,6 +179,5 @@ class _AddNewProductState extends State<AddNewProduct> {
     unitController.dispose();
     imgController.dispose();
   }
-
 }
 
