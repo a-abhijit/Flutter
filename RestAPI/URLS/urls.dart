4a528d737fbe1dc026028d8e3cc80be3edec18ee
Abhijit Saha <abhijit0045@gmail.com>
2025-09-03 20:08:13 +0600

    project

---
+++ RestAPI/URLS/urls.dart
@@ -0,0 +1,5 @@
+class Urls{
+  static const String _baseURL='http://35.73.30.144:2008/api/v1';
+  static const String getProductUrl='${_baseURL}/ReadProduct';
+
+}
--- RestAPI/homeScreen.dart
+++ RestAPI/homeScreen.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
+import 'package:demoapp/RestAPI/Models/product.dart';
+import 'package:demoapp/RestAPI/URLS/urls.dart';
 import 'package:demoapp/RestAPI/add_new_product.dart';
 import 'package:demoapp/RestAPI/update_product.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -10,6 +15,39 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  List<Product> _productList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getProduct();
+  }
+
+  Future<void> getProduct() async {
+    // 1. Define the URL
+    final url = Uri.parse('http://35.73.30.144:2008/api/v1/ReadProduct');
+    // 2. Send GET request
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final decodedJSON = jsonDecode(response.body);
+      for (Map<String, dynamic> productJson in decodedJSON['data']) {
+        Product product = Product();
+        product.id = productJson['_id'];
+        product.name = productJson['ProductName'];
+        product.code = productJson['ProductCode'];
+        product.image = productJson['Img'];
+        product.quantity = productJson['Qty'];
+        product.unitprice = productJson['UnitPrice'];
+        product.totalprice = productJson['TotalPrice'];
+        _productList.add(product);
+      }
+      setState(() {});
+    } else {
+      print("❌ Failed to fetch posts. Code: ${response.statusCode}");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +60,13 @@ class _HomescreenState extends State<Homescreen> {
         ),
       ),
       body: ListView.separated(
-        itemCount: 15,
+        itemCount: _productList.length,
         itemBuilder: (context, index) {
           return ListTile(
-            leading: CircleAvatar(),
+            leading: CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage(_productList[index].image),
+            ),
             trailing: PopupMenuButton<PopmenuOptions>(
               itemBuilder: (context) {
                 return [
@@ -40,25 +81,28 @@ class _HomescreenState extends State<Homescreen> {
                   ),
                 ];
               },
-              onSelected: (PopmenuOptions selected){
-                if (selected == PopmenuOptions.update){
+              onSelected: (PopmenuOptions selected) {
+                if (selected == PopmenuOptions.update) {
                   PopmenuOptions.update;
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>UpdateProduct()));
-                }else if(selected == PopmenuOptions.delete){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => UpdateProduct()),
+                  );
+                } else if (selected == PopmenuOptions.delete) {
                   print("delete");
                 }
               },
             ),
-            title: Text("Product name"),
+            title: Text(_productList[index].name),
             subtitle: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Code : 123456"),
+                Text("Code : ${_productList[index].code}"),
                 Row(
                   children: [
-                    Text("Quantity : 50"),
+                    Text("Quantity : ${_productList[index].quantity}"),
                     SizedBox(width: 10),
-                    Text("Unit price : 900"),
+                    Text("Unit price : ${_productList[index].unitprice}"),
                   ],
                 ),
               ],
